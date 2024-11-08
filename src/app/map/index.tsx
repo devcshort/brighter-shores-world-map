4a78253f09caa7c0093420e70a035ab602fa357f
devcshort <13677134+devcshort@users.noreply.github.com>
2024-11-08 15:36:36 -0800

    initial commit

---
+++ src/app/map/index.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { Map as LibreMap } from "maplibre-gl";
+import { useEffect, useRef } from "react";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export function Map() {
+  const bsmap = useRef(null);
+
+  useEffect(() => {
+    if (!bsmap.current) return;
+
+    const map = new LibreMap({
+      container: bsmap.current,
+      style: {
+        version: 8,
+        sources: {
+          "world-tiles": {
+            type: "raster",
+            tiles: ["/tiles/{z}/{x}/{y}.png"],
+            tileSize: 256,
+            minzoom: 0,
+            maxzoom: 5,
+          },
+        },
+        layers: [
+          {
+            id: "world-layer",
+            type: "raster",
+            source: "world-tiles",
+            paint: {
+              "raster-opacity": 1,
+            },
+          },
+        ],
+      },
+      center: [0, 0],
+      minZoom: 1.5,
+      maxZoom: 5,
+      attributionControl: {
+        customAttribution: "Brighter Shores Wiki",
+      },
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return (
+    <div
+      ref={bsmap}
+      className="absolute top-0 bottom-0 left-0 right-0 w-full h-full"
+    ></div>
+  );
+}
